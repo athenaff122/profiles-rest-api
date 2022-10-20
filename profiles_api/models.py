3dfc80d3c688513b48f3bbd045936e97fb23765f
JY 范 <jyfan@JYdeMacBook-Pro.local>
2022-10-19 22:40:46 -0400

    Enable Django admin for user profile model

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,3 +1,61 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import BaseUserManager
+
+
+
+class UserProfileManager(BaseUserManager):
+    "Manager for user profils"
+    def create_user(self, email, name, password=None):
+        "create a new user profile"
+        if not email:
+            raise ValueError('User must have an email address')
+
+        email=self.normalize_email(email)#把邮箱地址后半部分全变小写，前半部分case sensitive
+        user=self.model(email=email,name=name)
+
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+
+    def create_superuser(self, email, name, password):
+        "create and save a new super user with given details"
+        user=self.create_user(email,name,password)
+
+        user.is_superuser=True
+        user.is_staff=True
+        user.save(using=self._db)
+
+        return user
+
+
+class UserProfile(AbstractBaseUser,PermissionsMixin):
+    "database model for users in the system"
+    email=models.EmailField(max_length=255,unique=True)#不想要有两个user邮箱地址一样
+    name=models.CharField(max_length=255)
+    is_active=models.BooleanField(default=True)
+    is_staff=models.BooleanField(default=False)
+
+    objects=UserProfileManager()
+
+    USERNAME_FIELD='email'
+    REQUIRED_FIELDS=['name']
+
+    def get_full_name(self):
+        "retrieve full name of users"
+        return self.name
+
+    def get_short_name(self):
+        "retrieve short name of users"
+        return self.name
+
+    def __str__(self):
+        "return string representation of our user"
+        return self.email
 
 # Create your models here.
+
+#
